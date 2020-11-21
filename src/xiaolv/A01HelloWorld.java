98e4bbf7a512c844ab403e63c3e7fc276721b96a
moqi <moqimoqidea@gmail.com>
2020-11-21 10:49:47 +0800

    Add gitignore and HelloWorld

---
+++ src/xiaolv/A01HelloWorld.java
@@ -0,0 +1,14 @@
+package xiaolv;
+
+/**
+ * @author moqi
+ * On 11/21/20 10:46
+ */
+
+public class A01HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
