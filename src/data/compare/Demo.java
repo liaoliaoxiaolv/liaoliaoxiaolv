08e3795435382a8141845da1df33df277d1a16ef
moqi <moqimoqidea@gmail.com>
2020-11-27 23:04:03 +0800

    文件对比 补充参考

--- src/data/compare/Demo.java
+++ src/data/compare/Demo.java
@@ -3,6 +3,12 @@ package data.compare;
 /**
  * 文件对比
  *
+ * 参考：
+ * https://www.jetbrains.com/help/idea/local-history.html
+ * https://www.jetbrains.com/help/idea/comparing-files-and-folders.html
+ * https://www.jetbrains.com/help/idea/differences-viewer.html
+ * https://www.jetbrains.com/help/idea/differences-viewer-for-database-objects.html
+ *
  * @author moqi
  * On 11/27/20 22:59
  */
